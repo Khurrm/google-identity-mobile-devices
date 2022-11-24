168d2314608af09c264cf068224659abe14b88bf
Khurram <khurramworkspace@gmail.com>
2022-11-24 19:12:59 +0500

    Defining a function

--- GoogleApiFirst.py
+++ GoogleApiFirst.py
@@ -9,3 +9,11 @@ from google.oauth2 import service_account
 SCOPES = ['https://www.googleapis.com/auth/cloud-identity.devices']
 SERVICE_ACCOUNT_FILE = '/path/to/service-account-file.json'
 
+def create_service():
+  credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+  delegated_credentials = credentials.with_subject('mail@XXXX.com')
+
+  return service
+
+
